b40773ccd14ee3de5bd56ebcf25faff37a1a34ce
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-08-05 02:37:35 +0700

    update(other): :jigsaw: update
    pdate vite.config and create new .vercelignore

--- vite.config.ts
+++ vite.config.ts
@@ -211,7 +211,7 @@ export default defineConfig(async ({ mode }) => {
 			rollupOptions: {
 				output: {
 					chunkFileNames() {
-						return `[name].[hash].mjs}`
+						return `[name].[hash].js}`
 					},
 				},
 			},
